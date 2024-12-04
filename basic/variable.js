27ffacd854f257dfff14d31958ba1acbe33c0754
Bhavesh Patidar <bhaveshpatidarcs@gmail.com>
2024-12-04 10:23:56 +0000

    container config done

--- basic/variable.js
+++ basic/variable.js
@@ -2,6 +2,7 @@ const  accountId = 545;
 let accountEmail = "bhavesh@gmail.com";
 var accountPassword = "6465";
 accountcity = "hyderabad";
-
+let accountstate;
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountcity]);
+console.table([accountId,accountEmail,accountPassword,accountcity, accountstate]);
+
