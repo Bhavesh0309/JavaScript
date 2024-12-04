4f6d2fed526abd2669db016ea888b2630585d72e
Bhavesh Patidar <bhaveshpatidarcs@gmail.com>
2024-12-04 11:09:53 +0000

    container config done

---
+++ basic/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "bhavesh";
+let age = "20";
+let isLoogedin = true;
+
+//number => 2 to the power 40
+//bigint => big values 
+//string => ""
+//boolean => true or false
+//null => standalone value 
+//undefined =>  
+// and many more .........
